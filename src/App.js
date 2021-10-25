3684ba39333cf3a25fe0cadfff41391ceac6d939
cameliaaaa <jieyic1@student.unimelb.edu.au>
2021-10-25 14:44:22 +0800

    add contact&groups

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./style";
+import Header from "./header";
+import Main from "./main";
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
---
+++ src/header/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Head = styled.header`
+    display: flex;
+    justify-content: space-between;
+    height: 100px;
+    padding: 10px 20px;
+    color: rgb(20,167,240);
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 100px;
+    >div{
+        margin: 0 10px;
+    }
+    img{
+        height: 100px;
+    }
+`;
